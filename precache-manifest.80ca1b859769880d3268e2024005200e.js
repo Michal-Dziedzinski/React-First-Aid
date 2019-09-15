34412569f72be4aa8ba208522d35dea5937ff0e8
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2019-09-15 11:00:23 +0200

    Updates

---
+++ precache-manifest.80ca1b859769880d3268e2024005200e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0a68a5acf461c22a894e07443bee808",
+    "url": "/React-Grabber/static/media/barber.c0a68a5a.png"
+  },
+  {
+    "revision": "79f7ccfcdc8f34b788aa",
+    "url": "/React-Grabber/static/css/main.97f5b90a.chunk.css"
+  },
+  {
+    "revision": "09b65075ab454622f07e",
+    "url": "/React-Grabber/static/js/runtime~main.f589e342.js"
+  },
+  {
+    "revision": "ea8a3f6449645c6456f8a5858d673bcf",
+    "url": "/React-Grabber/static/media/star.ea8a3f64.png"
+  },
+  {
+    "revision": "929b5ff56f1e3cce4951",
+    "url": "/React-Grabber/static/js/2.2595e2c1.chunk.js"
+  },
+  {
+    "revision": "f1173d846f343b250fba6cefccd2e63c",
+    "url": "/React-Grabber/static/media/salad.f1173d84.png"
+  },
+  {
+    "revision": "3f05e4f20abb26c1a6f186c230967d4e",
+    "url": "/React-Grabber/static/media/logo-dark.3f05e4f2.png"
+  },
+  {
+    "revision": "dcbb057d0785d5a54986cb3371abb6b9",
+    "url": "/React-Grabber/static/media/mapa.dcbb057d.png"
+  },
+  {
+    "revision": "79f7ccfcdc8f34b788aa",
+    "url": "/React-Grabber/static/js/main.c225de4b.chunk.js"
+  },
+  {
+    "revision": "9c85416ab6e0a3eaab2f8ed9bd04ea34",
+    "url": "/React-Grabber/static/media/burger.9c85416a.png"
+  },
+  {
+    "revision": "2f36b4b93f9c6e84d66b949e361958f7",
+    "url": "/React-Grabber/static/media/bus.2f36b4b9.png"
+  },
+  {
+    "revision": "ad03cbdf819daf0261520141706aa0b8",
+    "url": "/React-Grabber/static/media/cinema.ad03cbdf.png"
+  },
+  {
+    "revision": "2f5ff790e994c1f78e19a95ed7d40fd4",
+    "url": "/React-Grabber/static/media/logo-white.2f5ff790.png"
+  },
+  {
+    "revision": "a2b40a320b2b8a85ec5b88c57c5dcdb3",
+    "url": "/React-Grabber/static/media/logo.a2b40a32.png"
+  },
+  {
+    "revision": "bbf1c10ebc94f716c7b4d9d2b6505714",
+    "url": "/React-Grabber/static/media/star-grey.bbf1c10e.png"
+  },
+  {
+    "revision": "929b5ff56f1e3cce4951",
+    "url": "/React-Grabber/static/css/2.abdf2a3e.chunk.css"
+  },
+  {
+    "revision": "cc42522e49e8e5c28481b74ab8b33a52",
+    "url": "/React-Grabber/index.html"
+  }
+];
