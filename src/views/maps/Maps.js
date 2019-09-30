97836785d8a9bc1ed3767c2d659a62d0cd84fb01
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2019-09-30 07:13:00 +0200

    Add map placeholder

--- src/views/maps/Maps.js
+++ src/views/maps/Maps.js
@@ -5,20 +5,22 @@ import mapImage from 'assets/mapa.png';
 const StyledMap = styled.div`
   background-image: url(${mapImage});
   background-size: cover;
-  padding-bottom: 500px;
+  padding-bottom: calc(100vh - 250px);
   position: absolute;
-  bottom: 140px;
+  top: 0px;
   width: 100%;
+  z-index: -1;
 `;
 
 const StyledDescription = styled.div`
   background-color: ${({ theme }) => theme.colorPrimary};
   color: ${({ theme }) => theme.colorWhite};
   position: absolute;
-  bottom: 70px;
+  bottom: 0px;
   width: 100%;
   text-align: center;
   padding: 20px;
+  z-index: -1;
 `;
 
 const StyledDescriptionTextBig = styled.p`
@@ -28,13 +30,31 @@ const StyledDescriptionTextBig = styled.p`
 const StyledDescriptionTextSmall = styled.p`
   font-size: 14px;
 `;
+const StyledPlaceholder = styled.div`
+  position: relative;
+  height: calc(100vh - 172px);
+  background-color: rgba(255, 255, 255, 0.8);
+`;
+
+const StyledPlaceholderDescription = styled.h1`
+  position: absolute;
+  color: ${({ theme }) => theme.colorPrimary};
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  text-align: center;
+`;
 
 export const Maps = () => (
   <>
-    <StyledMap />
-    <StyledDescription>
-      <StyledDescriptionTextBig>Białostocka 11, Białystok</StyledDescriptionTextBig>
-      <StyledDescriptionTextSmall>Total grabs: 6231</StyledDescriptionTextSmall>
-    </StyledDescription>
+    <StyledPlaceholder>
+      <StyledPlaceholderDescription>To Be Done...</StyledPlaceholderDescription>
+      <StyledMap />
+      <StyledDescription>
+        <StyledDescriptionTextBig>Białostocka 11, Białystok</StyledDescriptionTextBig>
+        <StyledDescriptionTextSmall>Total grabs: 6231</StyledDescriptionTextSmall>
+      </StyledDescription>
+    </StyledPlaceholder>
   </>
 );
